edceaf6fb1c2bf0f7ff41ee12d320193d5a25a94
Md. Sadman Hasan Talukder <133516078+sadmanHT@users.noreply.github.com>
2026-09-06 23:09:00 +0600

    Phase 7: expose persistence counterfactual and reporting APIs

--- evaluationlab/evaluationlab/__init__.py
+++ evaluationlab/evaluationlab/__init__.py
@@ -1,4 +1,11 @@
 from evaluationlab.adapter import adapt_benchmark_artifact, evidence_tags
+from evaluationlab.counterfactual import (
+    CounterfactualMetrics,
+    CounterfactualObservation,
+    CounterfactualPairResult,
+    adapt_counterfactual_observation,
+    score_counterfactual_consistency,
+)
 from evaluationlab.engine import EvaluationEngine
 from evaluationlab.metrics import (
     brier_score,
@@ -19,24 +26,42 @@ from evaluationlab.models import (
     ToolCallAssessment,
     ToolCallOutcome,
 )
+from evaluationlab.persistence import (
+    EvaluationRunMetadata,
+    ExperimentConfiguration,
+    PersistedEvaluation,
+    SqlEvaluationStore,
+)
+from evaluationlab.reporting import reliability_diagram_svg, write_reliability_diagram
 
 __all__ = [
     "AggregateEvaluation",
+    "CounterfactualMetrics",
+    "CounterfactualObservation",
+    "CounterfactualPairResult",
     "EvaluationCase",
     "EvaluationEngine",
     "EvaluationResult",
+    "EvaluationRunMetadata",
     "EvidenceUtility",
+    "ExperimentConfiguration",
     "FailureCategory",
+    "PersistedEvaluation",
     "RootCauseCode",
+    "SqlEvaluationStore",
     "ToolCallAssessment",
     "ToolCallOutcome",
     "adapt_benchmark_artifact",
+    "adapt_counterfactual_observation",
     "brier_score",
     "canonical_root_cause",
     "evidence_tags",
     "expected_calibration_error",
     "reliability_bins",
+    "reliability_diagram_svg",
+    "score_counterfactual_consistency",
     "score_efficiency",
     "score_evidence",
     "score_root_causes",
+    "write_reliability_diagram",
 ]
